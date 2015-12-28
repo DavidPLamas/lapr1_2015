096ec9a29597b2752d96168da0fbc960ceeba703
DavidPLamas <dlamas@partner-it-group.com>
2015-12-28 22:05:24 +0000

    Finalização dos testes

--- test/lapr1_2015/FileToolsTest.java
+++ test/lapr1_2015/FileToolsTest.java
@@ -43,11 +43,12 @@ public class FileToolsTest {
      */
     @Test
     public void testGetNumberOfLines() {
-        System.out.println("getNumberOfLines");
+        System.out.printf("%nTesting Filetools.getNumberOfLines...%n");
         File file = new File("filetest.txt");
-        int expResult = 3;
+        int expResult = 4;
         int result = FileTools.getNumberOfLines(file);
         assertEquals(expResult, result);
+        System.out.printf("End of testing FileTools.getNumberOfLines...%n");
     }
 
     /**
@@ -61,6 +62,20 @@ public class FileToolsTest {
         float[] expResult = {-3,-2,0,0};
         float[] result = FileTools.getObjectiveFunction(line, nrColumns);
         assertArrayEquals(expResult, result, 0.0F);
+        
+        line = "Z=5X1";
+        nrColumns = 4;
+        float[] expResult2 = {-5,0,0,0};
+        result = FileTools.getObjectiveFunction(line, nrColumns);
+        assertArrayEquals(expResult2, result, 0.0F);
+        
+        
+        line = "Z=-X2";
+        nrColumns = 4;
+        float[] expResult3 = {0,1,0,0};
+        result = FileTools.getObjectiveFunction(line, nrColumns);
+        assertArrayEquals(expResult3, result, 0.0F);
+        
         System.out.printf("End of testing FileTools.getObjectiveFunction...%n");
     }
 
@@ -72,22 +87,22 @@ public class FileToolsTest {
         System.out.printf("%nTesting FileTools.getRestriction...%n");
         String line = "3X1-2X2<=4";
         int matrixLine = 1;
-        int nrColumns = 4;
-        float[] expResult = {3,-2,1,4};
+        int nrColumns = 5;
+        float[] expResult = {3,-2,1,0,4};
         float[] result = FileTools.getRestriction(line, matrixLine, nrColumns);
         assertArrayEquals(expResult, result, 0.0F);
         
         line = "-2X2<=5";
-        matrixLine = 1;
-        nrColumns = 4;
-        float[] expResult2 = {0,-2,1,5};
+        matrixLine = 2;
+        nrColumns = 5;
+        float[] expResult2 = {0,-2,0,1,5};
         float[] result2 = FileTools.getRestriction(line, matrixLine, nrColumns);
         assertArrayEquals(expResult2, result2, 0.0F);
         
         line = "-2X1<=7";
         matrixLine = 1;
-        nrColumns = 4;
-        float[] expResult3 = {-2,0,1,7};
+        nrColumns = 5;
+        float[] expResult3 = {-2,0,1,0,7};
         float[] result3 = FileTools.getRestriction(line, matrixLine, nrColumns);
         assertArrayEquals(expResult3, result3, 0.0F);
         System.out.printf("End of testing FileTools.getRestriction...%n");
@@ -121,12 +136,10 @@ public class FileToolsTest {
     @Test
     public void testIsValid() {
         System.out.printf("%nTesting FileTools.isValid...%n");
-        File file = null;
-        boolean expResult = false;
+        File file = new File("filetest.txt");
+        boolean expResult = true;
         boolean result = FileTools.isValid(file);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
         System.out.printf("End of testing FileTools.isValid...%n");
     }
 
@@ -136,28 +149,12 @@ public class FileToolsTest {
     @Test
     public void testSaveToFile() {
         System.out.printf("%nTesting FileTools.saveToFile...%n");
-        String fileName = "";
-        String data = "";
-        boolean expResult = false;
+        String fileName = "outputfile.txt";
+        String data = "Test data.\r\nTest data 2.";
+        boolean expResult = true;
         boolean result = FileTools.saveToFile(fileName, data);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
         System.out.printf("End of testing FileTools.saveToFile...%n");
-    }
-
-    /**
-     * Test of getFileData method, of class FileTools.
-     */
-    @Test
-    public void testGetFileData() {
-        System.out.println("getFileData");
-        File file = null;
-        String expResult = "";
-        String result = FileTools.getFileData(file);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
--- test/lapr1_2015/Lapr1_2015Test.java
+++ test/lapr1_2015/Lapr1_2015Test.java
@@ -44,69 +44,16 @@ public class Lapr1_2015Test {
     @Test
     public void testFindZValue() {
         System.out.printf("%nTesting Lapr1_2015.findZValue...%n");
-        float[][] matrix = null;
-        String expResult = "";
+        float[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        String expResult = "Z = 4,00";
         String result = Lapr1_2015.findZValue(matrix);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-        System.out.printf("End of testing Lapr1_2015.findZValue...%n");
-    }
-    
-    /**
-     * Test of main method, of class Lapr1_2015.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        Lapr1_2015.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of simplexMethod method, of class Lapr1_2015.
-     */
-    @Test
-    public void testSimplexMethod() {
-        System.out.println("simplexMethod");
-        float[][] matrix = null;
-        File inputFile = null;
-        String outputFileName = "";
-        int nrVar = 0;
-        String inputFileData = "";
-        Lapr1_2015.simplexMethod(matrix, inputFile, outputFileName, nrVar, inputFileData);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of fillMatrix method, of class Lapr1_2015.
-     */
-    @Test
-    public void testFillMatrix() {
-        System.out.println("fillMatrix");
-        float[][] matrix = null;
-        File file = null;
-        Lapr1_2015.fillMatrix(matrix, file);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getFormattedMatrix method, of class Lapr1_2015.
-     */
-    @Test
-    public void testGetFormattedMatrix() {
-        System.out.println("getFormattedMatrix");
-        float[][] matrix = null;
-        int nrVar = 0;
-        String expResult = "";
-        String result = Lapr1_2015.getFormattedMatrix(matrix, nrVar);
+        
+        float[][] matrix2 = {{1,2,3,-32}};
+        expResult = "Z = -32,00";
+        result = Lapr1_2015.findZValue(matrix2);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.printf("End of testing Lapr1_2015.findZValue...%n");
     }
 
     /**
@@ -114,45 +61,23 @@ public class Lapr1_2015Test {
      */
     @Test
     public void testGetOutputLineFormat() {
-        System.out.println("getOutputLineFormat");
-        int nrColumns = 0;
-        String expResult = "";
+        System.out.println("%nTesting Lapr1_2015.getOutputLineFormat...%n");
+        int nrColumns = 5;
+        String expResult = "%8s|%8s|%8s|%8s|%8s%n";
         String result = Lapr1_2015.getOutputLineFormat(nrColumns);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getOutputMatrixHeader method, of class Lapr1_2015.
-     */
-    @Test
-    public void testGetOutputMatrixHeader() {
-        System.out.println("getOutputMatrixHeader");
-        int nrColumns = 0;
-        int nrVar = 0;
-        String lineFormat = "";
-        String[] lineArgs = null;
-        String expResult = "";
-        String result = Lapr1_2015.getOutputMatrixHeader(nrColumns, nrVar, lineFormat, lineArgs);
+        
+        nrColumns = 0;
+        expResult = "%n";
+        result = Lapr1_2015.getOutputLineFormat(nrColumns);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findVariableValues method, of class Lapr1_2015.
-     */
-    @Test
-    public void testFindVariableValues() {
-        System.out.println("findVariableValues");
-        float[][] matrix = null;
-        int nrVar = 0;
-        String expResult = "";
-        String result = Lapr1_2015.findVariableValues(matrix, nrVar);
+        
+        nrColumns = 1;
+        expResult = "%8s%n";
+        result = Lapr1_2015.getOutputLineFormat(nrColumns);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        System.out.println("End of testing Lapr1_2015.getOutputLineFormat...%n");
     }
     
 }
